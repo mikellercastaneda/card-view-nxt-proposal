85b245cf37b7e040c50696371b6e21c8fce4ce11
Mikeller C <mcastaneda@aptude.com>
2020-10-13 16:54:12 -0500

    Updates

---
+++ precache-manifest.4da04e19cd2c51e0ec9d61ac719dd73c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8072952ddecc8acab3d5760d565702d6",
+    "url": "/card-view-nxt-proposal/index.html"
+  },
+  {
+    "revision": "1f1630a119d46d0ab219",
+    "url": "/card-view-nxt-proposal/static/css/main.9d00d98c.chunk.css"
+  },
+  {
+    "revision": "dbc7780c3ae96872a4f0",
+    "url": "/card-view-nxt-proposal/static/js/2.8b80a19b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/card-view-nxt-proposal/static/js/2.8b80a19b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f1630a119d46d0ab219",
+    "url": "/card-view-nxt-proposal/static/js/main.2e991aec.chunk.js"
+  },
+  {
+    "revision": "d1f84c863c1da5c9c3e2",
+    "url": "/card-view-nxt-proposal/static/js/runtime-main.09bafb79.js"
+  }
+]);
