c6ecbcc7f9c79208d044737e2a3ee90703928bf0
Mikeller C <mcastaneda@aptude.com>
2020-10-23 16:21:22 -0500

    adding the card child component for the lanes

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store/store';
 //Components
-// import CardViewContainer from './components/CardViewContainer/CardViewContainer'
-import CardContainerInfinity from './components/CardContainerInfinity/CardContainerInfinity'
+import CardViewContainer from './components/CardViewContainer/CardViewContainer'
+// import CardContainerInfinity from './components/CardContainerInfinity/CardContainerInfinity'
 //Styling
 import './assets/styles/App.scss';
 
 function App() {
   return (
     <Provider store={store}>
-      {/* <CardViewContainer /> */}
-      <CardContainerInfinity />
+      <CardViewContainer />
+      {/* <CardContainerInfinity /> */}
     </Provider>
   );
 }
--- src/components/CardViewItem/CardViewItem.jsx
+++ src/components/CardViewItem/CardViewItem.jsx
@@ -5,29 +5,63 @@ import CardInput from '../CardInput/CardInput'
 import { uniqueId } from 'lodash-es';
 
 const CardViewItem = ({ title, item }) => {
-  const cardViewItemKey = uniqueId();
+  const cardKey = uniqueId();
   return (
-    <div className="card-view-item accordion">
-      <input type="checkbox" name="collapse" id={cardViewItemKey} defaultChecked />
+    <div className="card accordion">
+      <input type="checkbox" name="collapse" id={cardKey} defaultChecked />
       <h2 className="handle">
-        <label className="card-view-item_title" htmlFor={cardViewItemKey}>{title}</label>
+        <label className="card-title" htmlFor={cardKey}>{title}</label>
       </h2>
-      <div className="content">
-        <p className="card-view-item_sub-title">Hub Capacity</p>
-        <div className="card-view-item_dates-grid">
+      <div className="accordion-content">
+        <p className="card-subtitle">Hub Capacity</p>
+        <div className="card-dates-grid">
           {item
             .filter(({ name }) => name === 'hubCapacity')
             .map(({ dt, value }) =>
               <CardInput key={uniqueId()} label={dt} value={Number(value).toFixed(2)} />
             )}
         </div>
-        <div className="card-view-item_inputs-grid">
+        <div className="card-inputs-grid">
           {item
             .filter(({ name }) => name !== 'hubCapacity')
             .map(({ name, value }) =>
               <CardInput key={uniqueId()} label={name} value={value} />
             )}
         </div>
+
+        <div className="card-childs-grid">
+          <div className="card accordion">
+            <input type="checkbox" name="collapse" id="test" defaultChecked />
+            <h2 className="handle">
+              <label className="card-title" htmlFor="test">LABEL</label>
+            </h2>
+            <div className="accordion-content">
+              <div className="card-dates-grid">
+                <CardInput key={uniqueId()} label="label" value="0" />
+                <CardInput key={uniqueId()} label="label" value="0" />
+                <CardInput key={uniqueId()} label="label" value="0" />
+                <CardInput key={uniqueId()} label="label" value="0" />
+                <CardInput key={uniqueId()} label="label" value="0" />
+              </div>
+            </div>
+          </div>
+          <div className="card accordion">
+            <input type="checkbox" name="collapse" id="test" defaultChecked />
+            <h2 className="handle">
+              <label className="card-title" htmlFor="test">LABEL</label>
+            </h2>
+            <div className="accordion-content">
+              <div className="card-dates-grid">
+                <CardInput key={uniqueId()} label="label" value="0" />
+                <CardInput key={uniqueId()} label="label" value="0" />
+                <CardInput key={uniqueId()} label="label" value="0" />
+                <CardInput key={uniqueId()} label="label" value="0" />
+                <CardInput key={uniqueId()} label="label" value="0" />
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   );
